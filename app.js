48a9403a465a9069897aac8ae7556cbfd9b122bf
utilizador3 <160773752+utilizador3@users.noreply.github.com>
2024-02-21 18:07:41 +0000

    Update app.js
    Added a /test endpoint with "Hello, World!"

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.get("/test", (req, res) => res.send("Hello, World!"));
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
